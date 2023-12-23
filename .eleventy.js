c523ea2e7af8995fd1e78cd844f63681e670440c
alifeee <alifeeerenn@hotmail.com>
2023-12-23 21:12:21 +0000

    add toml file with content

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const toml = require("@iarna/toml");
+
 // .eleventy.js
 module.exports = function (eleventyConfig) {
+  // copy static content
   eleventyConfig.addPassthroughCopy({ public: "/" });
+
+  // parse TOML files
+  eleventyConfig.addDataExtension("toml", (contents) => toml.parse(contents));
 };
